0cfc65e07aebea7794dfdd719ec676ff46854333
Aeji Setonari <bluejaie.tesiorna@gmail.com>
2021-05-22 01:19:52 -0600

    Obfuscated client token. Not push clientToken.txt.

--- vredditBOT.py
+++ vredditBOT.py
@@ -1,14 +1,18 @@
-#import Disocrd Package
+#Discord Package
 import discord
 
 #Client (our bot)
 client = discord.Client()
 
+#ch_token = open('channeltoken.txt','r')
+#channeltoken = ch_token.readline()
+cl_token = open('clientToken.txt','r')
+clienttoken = cl_token.readline()
+
 @client.event
 async def on_ready():
-    #DO STUFF...
     dev_channel = client.get_channel(564326006903537679)
-    #await dev_channel.send('heehoo meatpog.')
+    await dev_channel.send('heehoo meatpog.')
 
 #Run the client on the server
-client.run('ODQ1NTI2MTk2MTQ0OTYzNTg0.YKiPog.Iywr1Ta_xkaHeEDlIkaWvdAJXvY')
+client.run(clienttoken)
