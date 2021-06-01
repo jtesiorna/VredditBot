32ca78726fa6e76178d857cedc1092a72afbaf09
Aeji Setonari <bluejaie.tesiorna@gmail.com>
2021-06-01 12:06:22 -0600

    Added status and updated readme file

--- chetbot.py
+++ chetbot.py
@@ -16,8 +16,51 @@ def cleanup_files(numgen):
     os.remove('ffmpeg2pass-0.log')
     os.remove('ffmpeg2pass-0.log.mbtree')
 
+#THIS CODE TAKEN FROM: https://stackoverflow.com/questions/64430805/how-to-compress-video-to-target-size-by-python
+def compress_video(video_full_path, output_file_name, target_size, numgen):
+    video_full_path = '/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4'
+    output_file_name = 'vredditcompress_' + numgen + '.mp4'
+    target_size = 8000
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+    probe = None
+    try:
+        probe = ffmpeg.probe(video_full_path)
+    except ffmpeg.Error as e:
+        print(e.stderr)
+        exit()
+    # Video duration, in s.
+    duration = float(probe['format']['duration'])
+    # Audio bitrate, in bps.
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+
+    # Target total bitrate, in bps.
+    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
+
+    # Target audio bitrate, in bps
+    if 10 * audio_bitrate > target_total_bitrate:
+        audio_bitrate = target_total_bitrate / 10
+        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+            audio_bitrate = min_audio_bitrate
+        elif audio_bitrate > max_audio_bitrate:
+            audio_bitrate = max_audio_bitrate
+    # Target video bitrate, in bps.
+    video_bitrate = target_total_bitrate - audio_bitrate
+
+    i = ffmpeg.input(video_full_path)
+    ffmpeg.output(i, os.devnull,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                  ).overwrite_output().run()
+    ffmpeg.output(i, output_file_name,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                  ).overwrite_output().run()
+
 #EVENTS:
 @client.event
+async def on_ready():
+    print("Ready!")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Reddit videos!"))
+
 async def on_message(message):
     numgen = str(uuid.uuid4())
     if message.author.id == client.user.id:
@@ -81,45 +124,3 @@ async def on_message(message):
         return
 #Run the client on the server
 client.run(clienttoken)
-
-
-
-
-#THIS CODE TAKEN FROM: https://stackoverflow.com/questions/64430805/how-to-compress-video-to-target-size-by-python
-def compress_video(video_full_path, output_file_name, target_size, numgen):
-    video_full_path = '/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4'
-    output_file_name = 'vredditcompress_' + numgen + '.mp4'
-    target_size = 8000
-    min_audio_bitrate = 32000
-    max_audio_bitrate = 256000
-    probe = None
-    try:
-        probe = ffmpeg.probe(video_full_path)
-    except ffmpeg.Error as e:
-        print(e.stderr)
-        exit()
-    # Video duration, in s.
-    duration = float(probe['format']['duration'])
-    # Audio bitrate, in bps.
-    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
-
-    # Target total bitrate, in bps.
-    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
-
-    # Target audio bitrate, in bps
-    if 10 * audio_bitrate > target_total_bitrate:
-        audio_bitrate = target_total_bitrate / 10
-        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
-            audio_bitrate = min_audio_bitrate
-        elif audio_bitrate > max_audio_bitrate:
-            audio_bitrate = max_audio_bitrate
-    # Target video bitrate, in bps.
-    video_bitrate = target_total_bitrate - audio_bitrate
-
-    i = ffmpeg.input(video_full_path)
-    ffmpeg.output(i, os.devnull,
-                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
-                  ).overwrite_output().run()
-    ffmpeg.output(i, output_file_name,
-                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
-                  ).overwrite_output().run()
--- readme.md
+++ readme.md
@@ -1,9 +1,18 @@
 Simple discord bot to save my homies one click.
-I hate that I have to click on the reddit link and go to the site itself to watch a reddit-hosted video. This bot:
- - Scans messages for a reddit link.
- - Once it finds one, it looks at the .json data for a v.redd.it link.
- - It then uses youtube-dl to download the video
- - Compresses the video to less than 8MB so we can
- - Upload to Discord as a reply to the original reddit link so users can just watch the video within Discord
+I didn't like that we had to go outside of Discord whenever someone shared a Reddit-hosted video link just to watch it so I made a bot for it.
+Chet basically provides a video for you to watch within Discord so you don't have to click into that one link.
+
+How to use Chet:
+You don't need to do anything special. Just post any Reddit links as long as it's a video hosted on Reddit. After a couple seconds, Chet will post a video as a reply to your original message.
+Here are some examples of valid links:
+https://old.reddit.com/r/TikTokCringe/comments/lsyww3/genius/
+https://www.reddit.com/r/TikTokCringe/comments/lsyww3/genius/
+https://v.redd.it/expmjnq62uj61
+
+
+Limitations:
+Because bots are only allowed to upload 8MB, often times we need to compress these videos down to fit that restriction. For the most part, the compression isn't too noticeable.
+However, this means that if the video you're sharing is more than 2-3 minutes or if it's especially high res, there's a highly likelihood that it will come out a bit more pixelated than the original link.
+
 
 o(一︿一+)o
