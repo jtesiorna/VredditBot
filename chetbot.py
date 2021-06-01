0659cd30cdacd7121898f1e22bef5df0c287a39d
Aeji Setonari <bluejaie.tesiorna@gmail.com>
2021-06-01 09:22:48 -0600

    Cleaned it up a little. -Made a proper function for the compressor -Made a function for cleanup

--- chetbot.py
+++ chetbot.py
@@ -13,6 +13,7 @@ client = discord.Client()
 #private information:
 with open('clientToken.txt','r') as cl_token:
     clienttoken = cl_token.read()
+numgen = str(uuid.uuid4())
 
 #EVENTS:
 @client.event
@@ -26,9 +27,6 @@ async def on_message(message):
     discord_message = message.content
     reg_match = re.search(reg_pattern, discord_message)
     vreddit_match = re.search(vreddit_pattern, discord_message)
-    numgen = str(uuid.uuid4())
-
-
 
     if reg_match:
         url_address = message.content + '.json'
@@ -48,45 +46,7 @@ async def on_message(message):
             if vreddit_size >= 8388608:
 #--------------------------------------------
 #COMPRESS VIDEO TO <8MB
-                #THIS CODE TAKEN FROM: https://stackoverflow.com/questions/64430805/how-to-compress-video-to-target-size-by-python
-                def compress_video(video_full_path, output_file_name, target_size):
-                    video_full_path = '/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4'
-                    output_file_name = 'vredditcompress_' + numgen + '.mp4'
-                    target_size = 8000
-                    min_audio_bitrate = 32000
-                    max_audio_bitrate = 256000
-                    probe = None
-                    try:
-                        probe = ffmpeg.probe(video_full_path)
-                    except ffmpeg.Error as e:
-                        print(e.stderr)
-                        exit()
-                    # Video duration, in s.
-                    duration = float(probe['format']['duration'])
-                    # Audio bitrate, in bps.
-                    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
-
-                    # Target total bitrate, in bps.
-                    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
-
-                    # Target audio bitrate, in bps
-                    if 10 * audio_bitrate > target_total_bitrate:
-                        audio_bitrate = target_total_bitrate / 10
-                        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
-                            audio_bitrate = min_audio_bitrate
-                        elif audio_bitrate > max_audio_bitrate:
-                            audio_bitrate = max_audio_bitrate
-                    # Target video bitrate, in bps.
-                    video_bitrate = target_total_bitrate - audio_bitrate
-
-                    i = ffmpeg.input(video_full_path)
-                    ffmpeg.output(i, os.devnull,
-                                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
-                                  ).overwrite_output().run()
-                    ffmpeg.output(i, output_file_name,
-                                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
-                                  ).overwrite_output().run()
-                compress_video('input.mp4', 'output.mp4', 50 * 1000)
+                compress_video('input.mp4', 'output.mp4', 50 * 1000, numgen)
 
                 file = discord.File(r'/mnt/d/Documents/Bot/vredditcompress_' + numgen + '.mp4')
                 sender = message.author
@@ -100,9 +60,7 @@ async def on_message(message):
                 await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here's a direct link to the post comments: "+"<"+vreddit_url+">", mention_author = False)
 #--------------------------------------------
 #CLEANUP DIRECTORY
-            os.remove('vredditvid_' + numgen + '.mp4')
-            os.remove('ffmpeg2pass-0.log')
-            os.remove('ffmpeg2pass-0.log.mbtree')
+            cleanup()
         else:
             return
 
@@ -114,44 +72,7 @@ async def on_message(message):
 
         vreddit_size = os.path.getsize('/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4')
         if vreddit_size >= 8388608:
-            def compress_video(video_full_path, output_file_name, target_size):
-                video_full_path = '/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4'
-                output_file_name = 'vredditcompress_' + numgen + '.mp4'
-                target_size = 8000
-                min_audio_bitrate = 32000
-                max_audio_bitrate = 256000
-                probe = None
-                try:
-                    probe = ffmpeg.probe(video_full_path)
-                except ffmpeg.Error as e:
-                    print(e.stderr)
-                    exit()
-                # Video duration, in s.
-                duration = float(probe['format']['duration'])
-                # Audio bitrate, in bps.
-                audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
-
-                # Target total bitrate, in bps.
-                target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
-
-                # Target audio bitrate, in bps
-                if 10 * audio_bitrate > target_total_bitrate:
-                    audio_bitrate = target_total_bitrate / 10
-                    if audio_bitrate < min_audio_bitrate < target_total_bitrate:
-                        audio_bitrate = min_audio_bitrate
-                    elif audio_bitrate > max_audio_bitrate:
-                        audio_bitrate = max_audio_bitrate
-                # Target video bitrate, in bps.
-                video_bitrate = target_total_bitrate - audio_bitrate
-
-                i = ffmpeg.input(video_full_path)
-                ffmpeg.output(i, os.devnull,
-                              **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
-                              ).overwrite_output().run()
-                ffmpeg.output(i, output_file_name,
-                              **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
-                              ).overwrite_output().run()
-            compress_video('input.mp4', 'output.mp4', 50 * 1000)
+            compress_video('input.mp4', 'output.mp4', 50 * 1000, numgen)
 
             file = discord.File(r'/mnt/d/Documents/Bot/vredditcompress_' + numgen + '.mp4')
             sender = message.author
@@ -161,10 +82,53 @@ async def on_message(message):
             file = discord.File(r'/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4')
             sender = message.author
             await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here's a direct link to the post comments: "+"<"+raw_vreddit_url+">", mention_author = False)
-        os.remove('vredditvid_' + numgen + '.mp4')
-        os.remove('ffmpeg2pass-0.log')
-        os.remove('ffmpeg2pass-0.log.mbtree')
+        cleanup()
     else:
         return
 #Run the client on the server
 client.run(clienttoken)
+
+
+async def cleanup():
+    os.remove('vredditvid_' + numgen + '.mp4')
+    os.remove('ffmpeg2pass-0.log')
+    os.remove('ffmpeg2pass-0.log.mbtree')
+
+#THIS CODE TAKEN FROM: https://stackoverflow.com/questions/64430805/how-to-compress-video-to-target-size-by-python
+async def compress_video(video_full_path, output_file_name, target_size, numgen):
+    video_full_path = '/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4'
+    output_file_name = 'vredditcompress_' + numgen + '.mp4'
+    target_size = 8000
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+    probe = None
+    try:
+        probe = ffmpeg.probe(video_full_path)
+    except ffmpeg.Error as e:
+        print(e.stderr)
+        exit()
+    # Video duration, in s.
+    duration = float(probe['format']['duration'])
+    # Audio bitrate, in bps.
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+
+    # Target total bitrate, in bps.
+    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
+
+    # Target audio bitrate, in bps
+    if 10 * audio_bitrate > target_total_bitrate:
+        audio_bitrate = target_total_bitrate / 10
+        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+            audio_bitrate = min_audio_bitrate
+        elif audio_bitrate > max_audio_bitrate:
+            audio_bitrate = max_audio_bitrate
+    # Target video bitrate, in bps.
+    video_bitrate = target_total_bitrate - audio_bitrate
+
+    i = ffmpeg.input(video_full_path)
+    ffmpeg.output(i, os.devnull,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                  ).overwrite_output().run()
+    ffmpeg.output(i, output_file_name,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                  ).overwrite_output().run()
