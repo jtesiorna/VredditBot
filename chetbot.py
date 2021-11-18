7c5dd85fa673bcf83bd11c9f3a5ce7deec5b294f
Aeji Setonari <bluejaie.tesiorna@gmail.com>
2021-11-17 22:05:01 -0700

    Chet is more polite now

--- chetbot.py
+++ chetbot.py
@@ -4,6 +4,7 @@ import discord, requests, json, uuid, os, re, ffmpeg, youtube_dl, urllib, sqlite
 from urllib.request import urlopen
 from discord.ext import commands
 from sys import argv
+import random
 
 #-----------------------------------------------------------------------------
 #Global variables
@@ -31,8 +32,12 @@ async def on_message(message):
     reg_pattern = 'https://(old\.|new\.|www\.)?reddit\.com/r/([\w.,@^=%&:/~+#-]*[\w@^=%&/~+#-])?'
     vreddit_pattern = 'https://v\.redd\.it/([\w.,@^=%&:/~+#-]*[\w@^=%&/~+#-])?'
     discord_message = message.content
+    tc_ss = "thanks chet"
+    tc = discord_message.lower()
     reg_match = re.search(reg_pattern, discord_message)
     vreddit_match = re.search(vreddit_pattern, discord_message)
+
+
 
     if reg_match:
         raw_reg_url = 'https://www.reddit.com/r/' + reg_match.group(2)
@@ -44,6 +49,8 @@ async def on_message(message):
 
 
         vreddit_url = json_data[0]['data']['children'][0]['data']['url']
+        post_title = json_data[0]['data']['children'][0]['data']['title']
+
         #if vreddit.url.startswith = query sqlite database
         #elif do the stuff below
         if vreddit_url.startswith('https://v.redd.it'):
@@ -56,17 +63,39 @@ async def on_message(message):
                 compress_video('input.mp4', 'output.mp4', 50 * 1000, numgen)
                 file = discord.File(r'/mnt/d/Documents/Bot/vredditcompress_' + numgen + '.mp4')
                 sender = message.author
-                await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here's a direct link to the post comments: "+"<"+vreddit_url+">", mention_author = False)
+                await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here are the post comments: \n" + "Title: **" + post_title + "**" +"\n<" + vreddit_url + ">", mention_author = False)
                 os.remove('vredditcompress_' + numgen + '.mp4')
 
             else:
                 file = discord.File(r'/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4')
                 sender = message.author
-                await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here's a direct link to the post comments: "+"<"+vreddit_url+">", mention_author = False)
+                await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here are the post comments: \n" + "Title: **" + post_title + "**" +"\n<" + vreddit_url + ">", mention_author = False)
                 #add vreddit_url and discord link to table
                 discordapp_url = '1'
                 db_connect(db_path=DEFAULT_PATH).execute("insert into contacts (name, phone, email) values (?, ?, ?)",(vreddit_url,discordapp_url))
             cleanup_files(numgen)
+        else:
+            return
+
+    elif tc_ss in tc:
+        ch = random.randint(1, 8)
+
+        if ch==1:
+            await message.reply(content="No problemo", mention_author = False)
+        elif ch==2:
+            await message.reply(content="No one asked", mention_author = False)
+        elif ch==3:
+            await message.reply(content="You're welcome!", mention_author = False)
+        elif ch==4:
+            await message.reply(content="My pleasure", mention_author = False)
+        elif ch==5:
+            await message.reply(content="Don't really have a choice <:stare:886855454481846332>", mention_author = False)
+        elif ch==6:
+            await message.reply(content="<:stare:886855454481846332>", mention_author = False)
+        elif ch==7:
+            await message.reply(content="Ok..?", mention_author = False)
+        elif ch==8:
+            await message.reply(content="How 'bout you fuck me instead?'", mention_author = False)
         else:
             return
 
@@ -82,13 +111,14 @@ async def on_message(message):
 
             file = discord.File(r'/mnt/d/Documents/Bot/vredditcompress_' + numgen + '.mp4')
             sender = message.author
-            await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here's a direct link to the post comments: "+"<"+raw_vreddit_url+">", mention_author = False)
+            await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here are the post comments: \n" + "<" + raw_vreddit_url + ">", mention_author = False)
             os.remove('vredditcompress_' + numgen + '.mp4')
         else:
             file = discord.File(r'/mnt/d/Documents/Bot/vredditvid_' + numgen + '.mp4')
             sender = message.author
-            await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here's a direct link to the post comments: "+"<"+raw_vreddit_url+">", mention_author = False)
+            await message.reply(file=file, content="**Hey! I saw that you posted a Reddit-hosted video.** \nYou can stay and watch it here instead, but here are the post comments: \n" + "<" + raw_vreddit_url + ">", mention_author = False)
         cleanup_files(numgen)
+
     else:
         return
 
