94ef9a3a393cd20954847c61dce3d8f70d268f21
Aeji Setonari <bluejaie.tesiorna@gmail.com>
2021-11-19 13:47:21 -0700

    delete clienttoken and test

--- test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern = 'https://v\.redd\.it/([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?'
-message = 'https://v.redd.it/d7p8cwap9x171'
-
-match = re.search(pattern, message)
-matchedString = match.group(0)
-
-print(matchedString)
