70ca9b69d64ed2bf6181dd7c7b46f70a326a8c58
Nectarios Chroniaris <nchroniaris@gmail.com>
2024-06-01 02:05:48 -0600

    Add required intents block

--- src/chetbot/chetbot.py
+++ src/chetbot/chetbot.py
@@ -18,7 +18,10 @@ from compressvideo import compress_video
 from discord.ext import commands
 from thankreplies import thankreply
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), "database.discordurl")
 video_directory = "/tmp/downloads/"
 
